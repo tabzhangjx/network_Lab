b61124d88f6a02856266af707a4f28a791a437f4
tabzhangjx <tabzhangjx@outlook.com>
2018-12-11 10:38:29 +0800

    a

---
+++ Lab2/algor.py
@@ -0,0 +1,45 @@
+
+
+class router:
+	
+	'class a router, including datas and methods'
+
+	#data
+
+
+
+
+
+
+	#method
+	def __init__(self, name, , ):
+      self.name = name
+      self.route_list = []
+      Employee.empCount += 1
+
+
+
+    def update(self):
+
+
+
+    def send(self):
+
+    def __del__(self):
+      class_name = self.__class__.__name__
+      print class_name, "销毁"
+
+class net_w:
+
+
+
+	def __init__(self, name, , ):
+      self.name = name
+      self.route_list = []
+      Employee.empCount += 1
+
+
+
+	def __del__(self):
+      class_name = self.__class__.__name__
+      print class_name, "销毁"
