40b98493eb27a0c1d6dc54721ff0245c51724c87
tabzhangjx <tabzhangjx@outlook.com>
2018-12-13 15:01:57 +0800

    a

--- Lab2/algor.py
+++ Lab2/algor.py
@@ -1,7 +1,4 @@
-
-
 class router:
-	
 	'class a router, including datas and methods'
 
 	#data
@@ -12,35 +9,55 @@ class router:
   route_table=[]
 
 	#method
-	def __init__(self, name, , ):
+	def __init__(self, name):
     self.name=name
     num_ports=0
     ports=[]
     item=[]
+    item.append(name)
+    item.append(0)
+    item.append(0)
     route_table=[]
+    route_table.append(item)
+
+  def update_routing(self, update_routing_packet):
+    for i in update_routing_packet:
+      flag = 0
+      for j in route_table:
+        if i[1]==j[1]:
+          same_con(i, j)
+          flag=1
+          break
+      if flag==0:
+        diff_con(i, j)
+        
+
+  def send_urp(self):
+    return route_table
 
 
-  def update(self):
+  def send_data(self):
+    return "ping"
 
-
-
-  def send(self):
-
-  def __del__(self):
-    class_name = self.__class__.__name__
-    print class_name, "销毁"
+  def show_route_table(self):
+    for i in route_table:
+      print(i)
 
 class net_w:
+  'set of routers'
+
+  num_routers=0
+  router_set=[]
 
 
+	def __init__(self, name):
+    self.name = name
+    num_routers=0
+    router_set=[]
 
-	def __init__(self, name, , ):
-      self.name = name
-      self.route_list = []
-      Employee.empCount += 1
+  def creat_router(self, name):
 
 
+  def delete_router():
 
-	def __del__(self):
-      class_name = self.__class__.__name__
-      print class_name, "销毁"
+
