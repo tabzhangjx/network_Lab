ca537bcc9e107797441733b987524ce4a804616e
tabzhangjx <tabzhangjx@outlook.com>
2018-12-13 11:03:30 +0800

    a

--- Lab2/algor.py
+++ Lab2/algor.py
@@ -5,29 +5,30 @@ class router:
 	'class a router, including datas and methods'
 
 	#data
-
-
-
-
-
+  name='R'
+  num_ports=0
+  ports=[]
+  item=[]
+  route_table=[]
 
 	#method
 	def __init__(self, name, , ):
-      self.name = name
-      self.route_list = []
-      Employee.empCount += 1
+    self.name=name
+    num_ports=0
+    ports=[]
+    item=[]
+    route_table=[]
+
+
+  def update(self):
 
 
 
-    def update(self):
+  def send(self):
 
-
-
-    def send(self):
-
-    def __del__(self):
-      class_name = self.__class__.__name__
-      print class_name, "销毁"
+  def __del__(self):
+    class_name = self.__class__.__name__
+    print class_name, "销毁"
 
 class net_w:
 
